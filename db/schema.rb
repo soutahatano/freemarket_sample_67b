26e7cc5f02548cabdb2d10ab8f273d751e5fb47f
soutahatano <yuzuriha3836@gmail.com>
2020-01-29 10:23:56 +0900

    db をMySQLに変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_01_28_105431) do
 
-  create_table "demos", force: :cascade do |t|
+  create_table "demos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
