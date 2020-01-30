37057cedc35d709f5f858626f65ad422bac714d5
soutahatano <yuzuriha3836@gmail.com>
2020-01-30 11:49:47 +0900

    credencial.yaml config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   before_action :basic_auth
 
   private
 
+  def production?
+    Rails.env.production?
+  end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username == Rails.application.credentials[:basic][:auth_user] && password ==  Rails.application.credentials[:basic][:auth_password]
     end
   end
 end
