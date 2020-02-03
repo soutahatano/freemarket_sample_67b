2d51500bab5cd79a2266b7873dc15a578062d0fe
soutahatano <yuzuriha3836@gmail.com>
2020-02-03 16:12:53 +0900

    chenge roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "items#new"
+  root "items#index"
   resources :items
   resources :users
 end
