b4bb8d22dd551a9175380397d1f7424a750e6379
soutahatano <yuzuriha3836@gmail.com>
2020-02-07 16:15:44 +0900

    エラー文表示

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,19 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  VALID_NAME_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  VALID_PHONE_REGEX = /\A\d{10,11}\z/
+  VALID_POST_REGEX = /\A\d{3}[-]\d{4}\z/
+
+  validates :last_name,             presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name,            presence: true, format: { with: VALID_NAME_REGEX }
+  validates :last_name_kana,        presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :first_name_kana,       presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :prefecture,            presence: true
+  validates :city,                  presence: true
+  validates :house_number,          presence: true
+  validates :post_number,           presence: true, format: { with: VALID_POST_REGEX  }
+  validates :phone_number,          format: { with: VALID_PHONE_REGEX  }, allow_blank: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,18 @@ class User < ApplicationRecord
   has_one :address, dependent: :destroy
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  PASSWORD_VALIDATION = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,128}+\z/i
+  VALID_NAME_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  VALID_PHONE_REGEX = /\A\d{10,11}\z/
+
   validates :email,                 presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
-  validates :password,              presence: true, length: {minimum: 7, maximum: 128},    format: { with: PASSWORD_VALIDATION }
-  validates :password_confirmation, presence: true, length: {minimum: 7, maximum: 128}
   validates :nickname,              presence: true, length: {maximum: 20}
+  validates :last_name,             presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name,            presence: true, format: { with: VALID_NAME_REGEX }
+  validates :last_name_kana,        presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :first_name_kana,       presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :birthday,              presence: true
+
+  validates :phone_number,          presence: true, format: { with: VALID_PHONE_REGEX  }
+
 end
