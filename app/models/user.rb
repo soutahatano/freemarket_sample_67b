d498c85844aadb3583e9c914b6adca9c48dcb04d
soutahatano <yuzuriha3836@gmail.com>
2020-02-14 02:16:31 +0900

    クレジット登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_one :address, dependent: :destroy
+  has_one :credit,  dependent: :destroy
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_NAME_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
