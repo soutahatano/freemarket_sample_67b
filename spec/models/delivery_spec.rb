848e1c3b0d87877da0b68b005a164c3c3760ea82
soutahatano <yuzuriha3836@gmail.com>
2020-02-18 04:01:54 +0900

    単体テスト

--- spec/models/delivery_spec.rb
+++ spec/models/delivery_spec.rb
@@ -30,5 +30,37 @@ describe Delivery, type: :model do
       delivery.valid?
       expect(delivery.errors[:prefecture_id]).to include('を入力してください')
     end
+
+  end
+  describe '#update' do
+
+    it 'is invalid without a item' do
+      delivery = build(:delivery)
+      delivery.update(item_id: '')
+      delivery.valid?
+      expect(delivery.errors[:item_id]).to include("を入力してください")
+    end
+
+    it 'is invalid without a deliverycharge_id' do
+      delivery = build(:delivery)
+      delivery.update(deliverycharge_id: '')
+      delivery.valid?
+      expect(delivery.errors[:deliverycharge_id]).to include('を入力してください')
+    end
+    
+    it 'is invalid without a deliveryday_id' do
+      delivery = build(:delivery)
+      delivery.update(deliveryday_id: '')
+      delivery.valid?
+      expect(delivery.errors[:deliveryday_id]).to include('を入力してください')
+    end
+    
+    it 'is invalid without a prefecture_id' do
+      delivery = build(:delivery)
+      delivery.update(prefecture_id: '')
+      delivery.valid?
+      expect(delivery.errors[:prefecture_id]).to include('を入力してください')
+    end
+    
   end
 end
