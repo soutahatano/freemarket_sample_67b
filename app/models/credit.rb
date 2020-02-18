80ab039e19d4ec35ef508705343155eacae0b671
soutahatano <yuzuriha3836@gmail.com>
2020-02-18 12:39:06 +0900

    バリデーションをまとめる

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,7 +1,5 @@
 class Credit < ApplicationRecord
   belongs_to :user
 
-  validates :user_id,        presence: true
-  validates :customer_id,    presence: true
-  validates :card_id,        presence: true
+  validates :user_id, :customer_id, :card_id, presence: true
 end
