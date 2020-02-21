453358cec8bab4bb2e277ab3f8d4ef672e36cce4
soutahatano <yuzuriha3836@gmail.com>
2020-02-21 10:48:29 +0900

    seed

--- db/seeds/user.rb
+++ db/seeds/user.rb
@@ -1,4 +1,4 @@
-User.create(
+user = User.create(
 nickname: "Mario",
 email: "test@test.test",
 password: "testest",
@@ -9,3 +9,16 @@ first_name_kana: "フリ",
 last_name_kana: "マリオ",
 birthday: "1964-11-05",
 phone_number: "09099009900")
+
+Address.create(
+  user: user.id,
+  first_name: "不離",
+  last_name: "真理雄",
+  first_name_kana: "フリ",
+  last_name_kana: "マリオ",
+  prefecture_id: "7",
+  city: "きのこ王国",
+  house_number: "1-2",
+  post_number: "470-1155",
+  phone_number: "09099009900"
+)
